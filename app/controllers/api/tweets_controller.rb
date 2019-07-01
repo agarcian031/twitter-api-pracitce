71fb026170cc45799f53a9d6db09650fa652e93b
Amanda Garcia <algarcian@outlook.com>
2019-07-01 11:26:11 -0600

    Fleshed out controllers and routes

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,4 +1,6 @@
 class Api::TweetsController < ApplicationController
   def index
+    # will go into the twitter_client.rb and then will rub the function home_timeline
+    render json: TwitterClient.home_timeline
   end
 end
